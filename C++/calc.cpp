9d4009a20532f00ad71e5a7b0d5dd06c76c55965
Josef Can <90180324+satoshei@users.noreply.github.com>
2022-01-18 11:51:17 +0100

    master

---
+++ C++/calc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Ange första nummer: ";
+    cin >> num1;
+
+    cout <<"Ange andra nummret: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+    return 0;
+
+}
